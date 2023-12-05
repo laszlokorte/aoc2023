222f9b00645343c56512e794b181a0b5eeb262e7
Laszlo Korte <9korte@informatik.uni-hamburg.de>
2023-12-05 23:31:02 +0100

    cleanup day5

--- lib/day5.ex
+++ lib/day5.ex
@@ -3,12 +3,34 @@ defmodule Day5 do
 
   @min_range -1
   @max_range 100_000
+  @blank_line_pattern ~r{\R\R}
+  @digit_pattern ~r{\d+}
+  @digit_pair_pattern ~r{(\d+) (\d+)}
+  @mapping_pattern ~r{(\d+) (\d+) (\d+)}
+
+  def pack_dense_ranges(ranges) do
+    ranges
+    |> Enum.sort_by(fn {_, dst_start.._} -> dst_start end)
+    |> (&Enum.concat([{@min_range..@min_range, @min_range..@min_range}], &1)).()
+    |> (&Enum.concat([&1, [{@max_range..@max_range, @max_range..@max_range}]])).()
+    |> Enum.chunk_every(2, 1)
+    |> Enum.flat_map(fn
+      [{src, as..ae}, {_, bs.._}] ->
+        cond do
+          bs in as..ae -> [{src, as..ae}]
+          true -> [{src, as..ae//1}, {ae + 1, (ae + 1)..(bs - 1)//1}]
+        end
+
+      [r] ->
+        [r]
+    end)
+  end
 
   def extract_mappings(mapping_lines) do
-    String.split(mapping_lines, ~r/\R\R/, trim: true)
+    String.split(mapping_lines, @blank_line_pattern, trim: true)
     |> Enum.map(fn
       block ->
-        Regex.scan(~r{(\d+) (\d+) (\d+)}, block, capture: :all_but_first)
+        Regex.scan(@mapping_pattern, block, capture: :all_but_first)
         |> Enum.map(fn [dst, src, len] ->
           {
             String.to_integer(src),
@@ -18,33 +40,22 @@ defmodule Day5 do
         end)
         |> Enum.map(fn {dst, src, len} ->
           {
-            Range.new(src, src + len - 1),
+            src,
             Range.new(dst, dst + len - 1)
           }
         end)
-        |> Enum.sort_by(fn {_, dst_start.._} -> dst_start end)
-        |> (&Enum.concat([{@min_range..@min_range, @min_range..@min_range}], &1)).()
-        |> (&Enum.concat([&1, [{@max_range..@max_range, @max_range..@max_range}]])).()
-        |> Enum.chunk_every(2, 1)
-        |> Enum.flat_map(fn
-          [{src, as..ae}, {_, bs.._}] ->
-            cond do
-              bs in as..ae -> [{src, as..ae}]
-              true -> [{src, as..ae//1}, {(ae + 1)..(bs - 1)//1, (ae + 1)..(bs - 1)//1}]
-            end
-
-          r ->
-            r
-        end)
+        |> pack_dense_ranges
     end)
   end
 
-  def extract_seeds(seed_line) do
-    Regex.scan(~r{\d+}, seed_line) |> Enum.map(fn [num] -> String.to_integer(num) end)
+  def extract_seeds_singlton(seed_line) do
+    Regex.scan(@digit_pattern, seed_line)
+    |> Enum.map(fn [num] -> String.to_integer(num) end)
+    |> Enum.map(fn num -> Range.new(num, num) end)
   end
 
   def extract_seed_range(seed_line) do
-    Regex.scan(~r{(\d+) (\d+)}, seed_line, capture: :all_but_first)
+    Regex.scan(@digit_pair_pattern, seed_line, capture: :all_but_first)
     |> Enum.map(fn
       [start, len] ->
         Range.new(
@@ -54,48 +65,44 @@ defmodule Day5 do
     end)
   end
 
-  def part1(input) do
-    [seed_line, mapping_lines] = String.split(input, ~r/\R\R/, trim: true, parts: 2)
-    seeds = extract_seeds(seed_line)
-    maps = extract_mappings(mapping_lines)
-
-    Enum.reduce(maps, seeds, fn
-      mapping, inputs ->
-        Enum.map(
-          inputs,
-          fn i ->
-            case Enum.find(mapping, fn {_, dst} -> i in dst end) do
-              {src_start.._, dst_start.._} -> i - dst_start + src_start
-              nil -> i
-            end
-          end
-        )
-    end)
-    |> Enum.min()
+  def clamp_range(a..b, min..max) do
+    max(a, min)..min(b, max)
   end
 
-  def part2(input) do
-    [seed_line, mapping_lines] = String.split(input, ~r/\R\R/, trim: true, parts: 2)
-    seeds = extract_seed_range(seed_line)
-    maps = extract_mappings(mapping_lines)
-
+  def find_dependencies(seeds, maps) do
     Enum.reduce(maps, seeds, fn
       mapping, inputs ->
         Enum.flat_map(inputs, fn
           i ->
             Enum.filter(mapping, fn {_, dst} -> not Range.disjoint?(i, dst) end)
-            |> Enum.map(fn {src_start.._, dst_start..dst_end} ->
+            |> Enum.map(fn {src_start, dst_start..dst_end} ->
               case clamp_range(i, dst_start..dst_end) do
                 start..nd -> (start - dst_start + src_start)..(nd - dst_start + src_start)
               end
             end)
         end)
     end)
+  end
+
+  def part1(input) do
+    [seed_line, mapping_lines] = String.split(input, @blank_line_pattern, trim: true, parts: 2)
+
+    seeds = extract_seeds_singlton(seed_line)
+    maps = extract_mappings(mapping_lines)
+
+    find_dependencies(seeds, maps)
     |> Enum.map(&Enum.min/1)
     |> Enum.min()
   end
 
-  def clamp_range(a..b, min..max) do
-    max(a, min)..min(b, max)
+  def part2(input) do
+    [seed_line, mapping_lines] = String.split(input, @blank_line_pattern, trim: true, parts: 2)
+
+    seeds = extract_seed_range(seed_line)
+    maps = extract_mappings(mapping_lines)
+
+    find_dependencies(seeds, maps)
+    |> Enum.map(&Enum.min/1)
+    |> Enum.min()
   end
 end
