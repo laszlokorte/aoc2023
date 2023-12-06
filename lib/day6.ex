016683946655b69c4d9a8feecfc4b54445097b8b
Laszlo Korte <9korte@informatik.uni-hamburg.de>
2023-12-06 17:12:43 +0100

    cleanup

--- lib/day6.ex
+++ lib/day6.ex
@@ -3,23 +3,26 @@ defmodule Day6 do
 
   @digits ~r{\d+}
 
-  def integers_between(a,b) do
-    round(:math.floor(b)-:math.ceil(a) + 1)
+  def integers_between({a, b}) do
+    round(:math.floor(b) - :math.ceil(a) + 1)
   end
 
-  def pq_zeros(p,q) do
-    {-p/2-:math.sqrt(p**2/4-q-1), -p/2+:math.sqrt(p**2/4-q-1)}
+  def pq_zeros(p, q) do
+    {-p / 2 - :math.sqrt(p ** 2 / 4 - q - 1), -p / 2 + :math.sqrt(p ** 2 / 4 - q - 1)}
+  end
+
+  def single_int([num]) do
+    String.to_integer(num)
   end
 
   def part1(input) do
     [timeline, distanceline] = String.split(input, "\n", limit: 1, trim: true)
-    times = Regex.scan(@digits, timeline) |> Enum.map(fn [num] -> String.to_integer(num) end)
-    distances =
-      Regex.scan(@digits, distanceline) |> Enum.map(fn [num] -> String.to_integer(num) end)
+    times = @digits |> Regex.scan(timeline) |> Enum.map(&single_int/1)
+    distances = @digits |> Regex.scan(distanceline) |> Enum.map(&single_int/1)
 
-    times 
-    |> Enum.zip_with(distances, &pq_zeros/2) 
-    |> Enum.map(fn {a,b} -> integers_between(a,b) end)
+    times
+    |> Enum.zip_with(distances, &pq_zeros/2)
+    |> Enum.map(&integers_between/1)
     |> Enum.reduce(1, &(&1 * &2))
   end
 
