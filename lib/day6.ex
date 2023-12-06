b083ca72ff32ddb1fd653f030f39b03108d8905b
Laszlo Korte <9korte@informatik.uni-hamburg.de>
2023-12-06 13:14:58 +0100

    reformat

--- lib/day6.ex
+++ lib/day6.ex
@@ -6,11 +6,22 @@ defmodule Day6 do
   def part1(input) do
     [timeline, distanceline] = String.split(input, "\n", limit: 1, trim: true)
     times = Regex.scan(@digits, timeline) |> Enum.map(fn [num] -> String.to_integer(num) end)
-    distances = Regex.scan(@digits, distanceline) |> Enum.map(fn [num] -> String.to_integer(num) end)
 
-    choices = times |> Enum.map(fn max_time -> 0..max_time |> Enum.map(fn hold -> (max_time-hold)*hold end) end)
-    result = Enum.with_index(choices) |> Enum.map(fn
-     {c, i} -> c |> Enum.filter(fn res -> res > Enum.at(distances, i) end) |> Enum.count end) |> Enum.reduce(1, &(&1*&2))
+    distances =
+      Regex.scan(@digits, distanceline) |> Enum.map(fn [num] -> String.to_integer(num) end)
+
+    choices =
+      times
+      |> Enum.map(fn max_time ->
+        0..max_time |> Enum.map(fn hold -> (max_time - hold) * hold end)
+      end)
+
+    result =
+      Enum.with_index(choices)
+      |> Enum.map(fn
+        {c, i} -> c |> Enum.filter(fn res -> res > Enum.at(distances, i) end) |> Enum.count()
+      end)
+      |> Enum.reduce(1, &(&1 * &2))
 
     result
   end
