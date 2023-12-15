50990e919ca2ae6e15ed26a8214e42ca28d39b97
Laszlo Korte <9korte@informatik.uni-hamburg.de>
2023-12-15 14:12:23 +0100

    update elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Aoc23.MixProject do
     [
       app: :aoc23,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
