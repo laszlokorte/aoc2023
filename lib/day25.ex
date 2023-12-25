1041d3c46d2f26056c7301cc0293b6fb1d028173
Laszlo Korte <me@laszlokorte.de>
2023-12-25 23:26:50 +0100

    add final answers

---
+++ lib/day25.ex
@@ -0,0 +1,30 @@
+defmodule Day25 do
+  use AOC, day: 25
+
+  @line_break_pattern ~r{\R}
+  @colon ": "
+  @spaces ~r{\s+}
+
+  def parse(input) do
+    input
+    |> String.split(@line_break_pattern, trim: true)
+    |> Enum.map(&String.split(&1, @colon, parts: 2, trim: true))
+    |> Enum.flat_map(fn
+      [from, tos] -> tos
+      |> String.split(@spaces, trim: true)
+      |> Enum.map(&({from, &1}))
+    end)
+  end
+
+  def part(1, input) do
+    input
+    |> parse
+    |> Enum.map(fn {a,b} -> "#{a} -- #{b}" end)
+    |> Enum.join("\n")
+    |> then(&"\n\ngraph G {\n#{&1}\n}")
+  end
+
+  def part(2, input) do
+    input
+  end
+end
